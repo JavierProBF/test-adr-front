21edb8844362c332de4f2231e5f6b6185756e906
Javier <jbaronfontanilla@gmail.com>
2026-03-09 18:39:40 -0500

    feat: mejorar estilos y estructura de componentes en la aplicación

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { SideMenu } from "../../components/side-menu/side-menu";
 import { CardsSelection } from "../../components/cards-selection/cards-selection";
-import { Chat } from "../../components/chat/chat";
 
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [SideMenu, CardsSelection, Chat],
+  imports: [CardsSelection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
